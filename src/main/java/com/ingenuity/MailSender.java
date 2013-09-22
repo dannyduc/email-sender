db87e1ba510389bf3d2a8ebfc0be8868c183a297
danny <danny.duc@gmail.com>
2013-09-22 00:20:19 -0700

    Use unicode

--- src/main/java/com/ingenuity/MailSender.java
+++ src/main/java/com/ingenuity/MailSender.java
@@ -3,11 +3,19 @@ package com.ingenuity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
 public class MailSender {
 
-    public static void main(String[] args) {
+
+
+    public static final String REGISTERED_SIGN = "\u00ae";              // ®
+    public static final String TRADE_MARK = "\u2122";                   // ™
+    public static final String LEFT_SINGLE_QUOTATION_MARK = "\u2018";   // ‘
+    public static final String RIGHT_SINGLE_QUOTATION_MARK = "\u2019";  // ’
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("mailhost.ingenuity.com");
         mailSender.setDefaultEncoding("UTF-8");
@@ -15,8 +23,8 @@ public class MailSender {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo("dnguyen@ingenuity.com");
         message.setFrom("dnguyen@ingenuity.com");
-        message.setSubject("Ingenuity® iReport™!");
-        message.setText("Welcome to Ingenuity® iReport™! Use ‘Contact Support’ link for help");
+        message.setSubject("Ingenuity\u00ae iReport\u2122!");
+        message.setText("Welcome to Ingenuity\u00ae iReport\u2122! Use \u2018Contact Support\u2019 link for help");
         message.setSentDate(new Date());
 
         System.out.println("sending message: " + message);
